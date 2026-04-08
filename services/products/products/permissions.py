cc067841b9b24cf35f2ac7d82767ed943bef85ec
Brianojunga <brianojunga3@gmail.com>
2026-04-08 16:29:33 +0300

    Provide RBAC for different parts of the permissions

---
+++ services/products/products/permissions.py
@@ -0,0 +1,39 @@
+from rest_framework import permissions
+
+def has_role(request, allowed_roles):
+    if not request.auth:
+        return False
+
+    roles = request.auth.get("roles", [])
+    return any(role in allowed_roles for role in roles)
+
+from rest_framework import permissions
+
+
+class IsAdminOrReadOnly(permissions.BasePermission):
+    """
+    - Everyone: Read access
+    - Admin/Agent: Write access
+    """
+
+    def has_permission(self, request, view):
+        # Allow read-only
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+
+        return has_role(request, ["admin", "agent"])
+
+    def has_object_permission(self, request, view, obj):
+        # Allow read-only
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return has_role(request, ["admin", "agent"])
+    
+
+class IsAdminOrAgent(permissions.BasePermission):
+    #Only admin or agent can access
+    def has_permission(self, request, view):
+        return has_role(request, ["admin", "agent"])
+    
